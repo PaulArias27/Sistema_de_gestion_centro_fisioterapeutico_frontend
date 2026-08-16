aa4c1fabaf6dc7faf896c0ef5d9143beb850dbcc
PaulArias27 <paularias2727@gmail.com>
2026-08-16 00:11:16 -0500

    PA: .env.example creado

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+console.log(import.meta.env.VITE_API_URL);
 const axiosClient = axios.create({
-    baseURL: "http://localhost:8080/api/",
+    baseURL: import.meta.env.VITE_API_URL,
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",
